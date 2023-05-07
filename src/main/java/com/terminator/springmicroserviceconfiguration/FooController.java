2d186d2eacf49661a111b79efc1fdf6f486eb952
berna39 <kalemajoseph39@gmail.com>
2023-05-07 10:54:49 +0200

    configuration with list of values

--- src/main/java/com/terminator/springmicroserviceconfiguration/FooController.java
+++ src/main/java/com/terminator/springmicroserviceconfiguration/FooController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class FooController {
 
@@ -13,6 +15,9 @@ public class FooController {
     @Value(("${app.description: the FooApp description}")) // If the placeholder doesn't exist, we need to set a default value
     private String description;
 
+    @Value("${app.akatsuki}")
+    List<String> akatsuki;
+
     @GetMapping("/")
     public String index(){
         return message;
@@ -21,5 +26,10 @@ public class FooController {
     @GetMapping("/description")
     public String description(){
         return description;
+    }
+
+    @GetMapping("/akatsuki")
+    public List<String> akatsuki(){
+        return akatsuki;
     }
 }
