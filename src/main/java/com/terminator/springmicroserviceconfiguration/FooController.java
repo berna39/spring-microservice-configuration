ee879a8bb0ee3f2b73513abb424731e2873ff544
berna39 <kalemajoseph39@gmail.com>
2023-05-07 11:07:11 +0200

    key-value pair

--- src/main/java/com/terminator/springmicroserviceconfiguration/FooController.java
+++ src/main/java/com/terminator/springmicroserviceconfiguration/FooController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FooController {
@@ -18,18 +19,26 @@ public class FooController {
     @Value("${app.akatsuki}")
     List<String> akatsuki;
 
+    @Value("#{${app.dbconfig}}")
+    Map<String, String> dbConfig;
+
     @GetMapping("/")
-    public String index(){
+    public String getMessage(){
         return message;
     }
 
     @GetMapping("/description")
-    public String description(){
+    public String getDescription(){
         return description;
     }
 
     @GetMapping("/akatsuki")
-    public List<String> akatsuki(){
+    public List<String> getAkatsuki(){
         return akatsuki;
+    }
+
+    @GetMapping("/config")
+    public Map<String, String> getConfig(){
+        return dbConfig;
     }
 }
